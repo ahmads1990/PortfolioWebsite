e297834af9e07961263c6cb71de08648238cbc88
ahmads1990 <ahmads20002000@gmail.com>
2023-12-27 19:33:57 +0200

    edited javascript

--- scripts.js
+++ scripts.js
@@ -7,7 +7,8 @@ function redirectToGoogleDrive() {
     window.location.href =
         "https://drive.google.com/file/d/1gtS48wI0ltcc-AuGmpzOlLRJjq7QCfIC/view?usp=sharing";
 }
-// re
+
+// Utility function to calculate the current theme setting.
 function calculateSettingAsThemeString({
     localStorageTheme,
     systemSettingDark,
@@ -22,45 +23,58 @@ function calculateSettingAsThemeString({
     }
     return "light";
 }
-// get theme on page load
-const localStorageTheme = localStorage.getItem("theme");
-// detect the user’s system settings
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
 
-// current user theme
-let currentThemeSetting = calculateSettingAsThemeString({
-    localStorageTheme,
-    systemSettingDark,
-});
-
-// target the button using the data attribute
-const button = document.querySelector("[data-theme-toggle]");
-const lightIcon = document.getElementById("light-icon");
-const darkIcon = document.getElementById("dark-icon");
-
-button.addEventListener("click", () => {
-    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-
+//Utility function to update the button and aria-label.
+function updateButton({ buttonEl, isDark }) {
     // update the button
-    const newCta = newTheme === "dark" ? "sun" : "moon";
+    const currentThemeIcon = isDark ? "sun icon" : "moon icon";
 
-    if (newTheme == "dark") {
+    if (isDark) {
         lightIcon.style.display = "block";
         darkIcon.style.display = "none";
     } else {
         lightIcon.style.display = "none";
         darkIcon.style.display = "block";
     }
-    // use an aria-label if you are omitting text on the button
-    // and using sun/moon icons, for example
-    button.setAttribute("aria-label", newCta);
+    buttonEl.setAttribute("aria-label", currentThemeIcon);
+}
+// Utility function to update the theme setting on the html
+function updateThemeOnHtmlEl({ theme }) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+}
 
-    // update theme attribute on HTML to switch theme in CSS
-    document.querySelector("html").setAttribute("data-theme", newTheme);
+/**
+ * On page load:
+ */
 
-    // update in local storage
+// target the button using the data attribute
+const button = document.querySelector("[data-theme-toggle]");
+const lightIcon = document.getElementById("light-icon");
+const darkIcon = document.getElementById("dark-icon");
+// get theme on page load
+const localStorageTheme = localStorage.getItem("theme");
+// detect the user’s system settings
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Work out the current site settings
+let currentThemeSetting = calculateSettingAsThemeString({
+    localStorageTheme,
+    systemSettingDark,
+});
+
+//Update the theme setting and button text according to current settings
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+/**
+ * 4. Add an event listener to toggle the theme
+ */
+button.addEventListener("click", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+    console.log(newTheme);
     localStorage.setItem("theme", newTheme);
+    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+    updateThemeOnHtmlEl({ theme: newTheme });
 
-    // update the currentThemeSetting in memory
     currentThemeSetting = newTheme;
 });
